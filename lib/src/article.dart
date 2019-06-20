02b26a9d4fbe69a3bc8abfa8daf285b127127b69
Abraao Levi <abr4ao.levi@gmail.com>
2019-06-20 17:14:20 -0300

    Add built_value

--- lib/src/article.dart
+++ lib/src/article.dart
@@ -1,27 +1,44 @@
-class Article {
-  final String title;
-  final String url;
-  final String by;
-  final int time;
-  final int score;
+library article;
 
-  Article(
-      {this.title,
-      this.url,
-      this.by,
-      this.time,
-      this.score});
+import 'dart:convert';
 
-  factory Article.fromJson(Map<String, dynamic> json) {
-    if (json == null) {
-      return null;
-    }
-    return Article(
-      title: json['title'],
-      url: json['url'],
-      by: json['by'],
-      time: json['time'],
-      score: json['score'] as int
-    );
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+
+import 'serializers.dart';
+
+part 'article.g.dart';
+
+abstract class Article implements Built<Article, ArticleBuilder> {
+
+  int get id;
+  bool get deleted;
+  String get type; // "job", "story", "comment", "poll", or "pollopt".
+  String get by;
+  int get time;
+  String get text;
+  bool get dead;
+  int get parent;
+  int get poll;
+  BuiltList<int> get kids;
+  String get url;
+  int get score;
+  String get title;
+  BuiltList<int> get parts;
+  int get descendants;
+
+  Article._();
+
+  factory Article([void Function(ArticleBuilder) updates]) = _$Article;
+
+  String toJson() {
+    return json.encode(serializers.serializeWith(Article.serializer, this));
   }
+
+  static Article fromJson(String jsonString) {
+    return serializers.deserializeWith(Article.serializer, json.decode(jsonString));
+  }
+
+  static Serializer<Article> get serializer =>  _$articleSerializer;
 }
